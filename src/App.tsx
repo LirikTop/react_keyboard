468f542466476e9b45546b0f512b43293d7176b4
Lirik <melnicukkiril2002@gmail.com>
2025-03-21 00:06:38 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  keyboard: string;
+}
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    keyboard: '',
+  };
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    this.setState({ keyboard: event.key });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.keyboard.length
+            ? `The last pressed key is [${this.state.keyboard}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
